b5df683d2f4438863ef909899d999f28200813fa
kimseewon <ksaxz12@naver.com>
2021-05-20 16:41:45 +0900

    520

--- js/main.js
+++ js/main.js
@@ -1,14 +1,98 @@
-var site = prompt("메인, 레시피, 푸드, 트레벌 중 \ 당신이 원하시는 정보는 무엇입니까","");
-var url;
+(function(win, $){
+	var $html = $("html");
+	var deviceSize = {
+		pc:1024,
+		tablet:768,
+		mobile:767
+	};
 
-switch(site){
-    case"메인":url = "http://127.0.0.1:5500/index.html"
-    break;
-    case"레시피":url = "http://127.0.0.1:5500/pages/recipes.html"
-    break;
-    case"푸드":url = "http://127.0.0.1:5500/pages/food.html"
-    break;
-    case"트레벌":url = "../pages/Travel.html"
-    break;
-    default:alert("보기 중에 없는 정보입니다.")
-}
+	function scrollShowHide(status) {
+		$html.css({"overflow-y":status});
+		return $html.width();    
+	}
+
+	var sc_w1 = scrollShowHide("hidden"), // 스크롤이 없을때 width
+	sc_w2 = scrollShowHide("scroll"),   // 스크롤이 있을때 width
+	sc_w3 = sc_w1 - sc_w2;
+
+	if(sc_w3 > 0) {
+		deviceSize.pc = deviceSize.pc -  sc_w3;
+		deviceSize.tablet = deviceSize.tablet -  sc_w3;
+		deviceSize.mobile = deviceSize.mobile -  sc_w3;
+	}
+    
+	// console.log(deviceSize.pc);
+
+	$(win).on("resize", function() {
+		var w_size = $(win).width();
+		
+		if(w_size >= deviceSize.pc && !$("html").hasClass("pc")) {
+			$html.removeClass("mobile tablet").addClass("pc");
+			scrollShowHide("scroll");
+		} 
+		
+		else if(w_size < deviceSize.pc && w_size >= deviceSize.tablet 
+		&& !$("html").hasClass("tablet")) {
+			$html.removeClass("mobile pc").addClass("tablet");
+			scrollShowHide("scroll");
+		} 
+		
+		else if(w_size <= deviceSize.mobile 
+		&& !$html.hasClass("mobile")) {
+			$html.removeClass("pc tablet").addClass("mobile");
+			var menu_pos = parseInt($(".mobile-menu-wrap").css("left"));
+
+			if(menu_pos >= 0) {
+							scrollShowHide("hidden");
+			}
+		}
+	});
+
+	$(function(){
+		$(win).trigger("resize"); // 사이트 로딩시 강제 실행
+
+		$(document).on("mouseover focus",  // 데스크톱, 태블릿 이벤트
+		".pc #gnb>ul>li>a, .tablet #gnb>ul>li>a", 
+		gnbPlay);
+
+		$(document).on("click",   // 모바일 상위 메뉴 터치시 이벤트
+		".mobile #gnb>ul>li:not(.no-sub)>a", 
+		gnbPlay);
+
+		function gnbPlay() {  // 하위 메뉴 슬라이드 업/다운 기능
+			var $ts = $(this);
+			if($("html").hasClass("mobile")) {
+				$(".mobile #gnb>ul>li>a").removeClass("on");
+				$("#gnb ul ul:visible").slideUp(300);
+
+				if($ts.next().is(":hidden")) {
+					$ts.addClass("on");
+					$ts.next().stop(true,true).slideDown(300);
+				}
+			} else {
+				$("#gnb ul ul:visible").slideUp(300);
+				$ts.next().stop(true,true).slideDown(300);
+			}
+		}
+
+		$(document).on("mouseleave",
+		".pc #gnb, .tablet #gnb", gnbleave);
+
+		function gnbleave() {
+			$("#gnb ul ul:visible").slideUp(300);
+			$("#gnb>ul>li>a").removeClass("on");
+		}
+
+		$(".mobile-menu-open button").on("click", function() {
+			$(".mobile-menu-wrap").animate({"left":0}, 200);
+			scrollShowHide("hidden");
+		});
+
+		$(".mobile-menu-close button").on("click", function() {
+			$(".mobile-menu-wrap").animate({"left":"-1000px"}, 200);
+			scrollShowHide("scroll");
+			gnbleave();
+		});
+	});
+
+}(window, jQuery));
